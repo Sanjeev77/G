a0944b522fa6ef047246befa64a886cf650f6238
Sanjeev77 <sanju.a25@gmail.com>
2025-09-15 20:23:26 +0530

    Add files via upload

--- js/productData.js
+++ js/productData.js
@@ -33,6 +33,17 @@ const allProductsData = [
         "rating": 4,
         "affiliateLink": "https://www.amazon.com/Schylling-Stress-Colors-Shipped-Randomly/dp/B076FGVCRH/ref=sr_1_2?dib=eyJ2IjoiMSJ9.UBI0TyBZyB2AP776C2M_juAiI1HAziIdmO78uGiaRknFTyaYu5r2CRKqW2kddbOB5xned4vRWKNnKl9vwfwL678HcE5JDkd3EaYlkItUNTRS-UnnQ9oj-j8J_modEOTW0Yp6OQApfFHe9885G4Z3OfiDzQozUwxPyax7Yw4tPiHAtidMyeArbUkkWMLY7TGrzEliMNJHZWUh8fV-ssQ-rUZw8DYJLyGEgp9euVD2u3tvNoBLDssZ4nNKZ1wpQzLcsHljBbOghnWXySauf12VLGHyQaoDACbOT7SC9_NXqSE.tb4FEcjS4MHmJ1QC8OHcIAsetqJwAH_jfnht0UeIjpw&dib_tag=se&keywords=toys&qid=1757864046&refinements=p_36%3A-1000%2Cp_n_g-1004151746091%3A5442388011&rnid=165794011&s=toys-and-games&sr=1-2&tag=giftonbudget-21",
         "featured": false
+    },
+    {
+        "id": 7005,
+        "title": "Toddler Boys Construction Truck Vehicle Toy Set with Play Mat and Die-Cast Toy Car, Transport Carrier Truck for Kids 3-5, Birthday Gifts Toys for 3-8 Year Old Boys",
+        "price": "$28.02",
+        "priceValue": 28.02,
+        "category": "toys-games",
+        "image": "https://m.media-amazon.com/images/I/812xnV-V-GL._AC_SX679_.jpg",
+        "rating": 4.1,
+        "affiliateLink": "https://www.amazon.com/NIPLOOOW-Toddler-Construction-Die-Cast-Birthday/dp/B0D7M51TRB/ref=sr_1_10?dib=eyJ2IjoiMSJ9.vHlNFP2KRluifVJJb9qIrtUzWRnVloch8dklKEqLeJccYnIOzX-hHXp9HU0jOWyzJ31Hq6l65xlpOW7j0oTD8etDXXTD7Cj7xGZIcu7auuJW1u63Gp6QBjpq6Jvb6wpzaQkTaKt1yMdaRwi94NPcX-xID_HZInEnxLyDswLjPMSsDij7Q63abiAf19X_qnr8EvZNr4bImJ6w3Fcad9JdWFyWPcRbMHB_i3s8SW8z-Rzd303DsF7Be57imgsmqm_bpSRKxK5ILkzkXcSz2eSyzJ-muyoK6uGbnSoKZVjAUto.vc_K7EuzPBmnArxlIhsaxCw6lycYb6Q1jUVk1ZAWnLo&dib_tag=se&keywords=toys&qid=1757947391&refinements=p_n_g-1004151746091%3A5442388011&rnid=165794011&sr=8-10&tag=giftonbudget-21",
+        "featured": false
     }
 ];
 
